09c154cfe68f8a81951e6d3e9969ea46988f26b9
d-kens <omondikens1@gmail.com>
2024-03-12 05:36:01 +0300

    artisan command to create users

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Receive user input into the user array
+        $user['name'] = $this->ask('Name of the new user');
+        $user['email'] = $this->ask('Email of the new user');
+        $user['password'] = $this->secret('Password of the new user');
+
+
+        // Assign the user a role
+        $roleName = $this->choice('Role of the new user', ['admin', 'editor'], 1);
+        $role = Role::where('name', $roleName)->first();
+
+        if(!$role) {
+            $this->error('Role not found');
+
+            return -1;
+        }
+
+
+        // data validation
+        $validator = Validator::make($user, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required']
+        ]);
+
+        if($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+
+            return -1;
+        }
+
+
+        // Hash password, create a new user and and populate the pivot table
+        DB::transaction(function() use($user, $role) {
+            $user['password'] = Hash::make($user['password']);
+            $newUser = User::create($user);
+            $newUser->roles()->attach($role->id);
+        });
+
+
+        $this->info('User '.$user['email']. ' created successfully');
+
+        return 0;
+    }
+}
